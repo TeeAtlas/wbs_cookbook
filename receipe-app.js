454a51a7701025c36149d15a26e77bb5d8931d08
Subaru Goto <subaru.dayo@gmail.com>
2023-12-01 14:11:05 +0100

    linking 2 html pages

---
+++ receipe-app.js
@@ -0,0 +1,25 @@
+import { getShoppingList } from './shoppingList.js';
+
+const shoppingList = document.querySelector('.shopping-list-container');
+const backHome = document.querySelector('#back-to-home');
+
+console.log('Hello');
+
+const render = () => shoppingList.innerHTML = getShoppingList();
+render()
+
+// const addBtn = document.querySelector('#add-btn');
+// addBtn.addEventListener('click', () =>
+//   {
+//     const inputField = document.querySelector('.input-field');
+//     const inputForm = document.querySelector('#list-form')
+//     inputField.style.display = 'flex';
+//   }
+// )
+
+// This function is to go back to landingPage
+const goBackHome = () => {
+  window.location.href = './landing.html';
+};
+
+backHome.addEventListener('click', goBackHome); 
