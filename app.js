3f3282dd49a7ae8ac3f2cf5f250dac39aec956d8
Tania Boterman <taniaboterman@gmail.com>
2023-12-01 13:40:37 +0100

    links added to landing

--- app.js
+++ app.js
@@ -14,3 +14,26 @@ render()
 //     inputField.style.display = 'flex';
 //   }
 // )
+
+
+// links addes to landing html
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const roastImage = document.getElementById("roast");
+
+        roastImage.addEventListener('click', () => {
+            window.location.href = "http://127.0.0.1:5500/wbs_cookbook/receipes.html";
+        });
+    });
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const redirectBtn = document.getElementById("chefVideosSection");
+
+            redirectBtn.addEventListener('click', () => {
+                window.location.href = "https://www.youtube.com/watch?v=UsD1MhKBmD4&t=232s";
+    
+    });
